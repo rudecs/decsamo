d7a828250b947b41434d9f7c4fd6e6745eba2108
Sergey Shubin svs1370 <sergey.shubin@digitalenergy.online>
2019-06-17 11:20:23 +0300

    Add validity parameter and improve module documentation

--- Modules/decs_jwt.py
+++ Modules/decs_jwt.py
@@ -15,17 +15,49 @@ DOCUMENTATION = '''
 module: decs_jwt
 short_description: Obtain access token to be used for authentication to DECS cloud controller
 description:
-     - Obtain JWT (Json Web Token) from the specified Oauth2 provider. This JWT can be used in subsequent DECS modules
+     - Obtain JWT (JSON Web Token) from the specified Oauth2 provider. This JWT can be used in subsequent DECS modules'
        invocations to authenticate them to the DECS cloud controller.
 version_added: "2.4"
 author: "Sergey Shubin (sergey.shubin@digitalenergy.online)"
 notes:
-     - Environment variables can be used to pass parameters to the module
+     - Environment variables can be used to pass parameters to the module (see options below for details).
      - Specified Oauth2 provider must be trusted by the DECS cloud controller on which JWT will be used.
-     - If you register module output as my_jwt, the JWT value is accessed as my_jwt.jwt
+     - 'If you register module output as I(my_jwt), the JWT value is accessed as I(my_jwt.jwt)'
 requirements:
-     - "python >= 2.6"
+     - python >= 2.6
+     - PyJWT module
      - requests module
+     - decs_utils library module
+options:
+    app_id:
+        description:
+        - 'Application ID for authenticating to the Oauth2 provider specified in I(oauth2_url).'
+        - 'If not found in the playbook or command line arguments, the value will be taken from DECS_APP_ID
+           environment variable.'
+        required: no
+    app_secret:
+        description:
+        - 'Application API secret used for authenticating to the Oauth2 provider specified in I(oauth2_url).'
+        - 'If not found in the playbook or command line arguments, the value will be taken from DECS_APP_SECRET
+           environment variable.'
+        required: no
+    oauth2_url:
+        description:
+        - 'URL of the oauth2 authentication provider to obtain JWT from.'
+        - If not specified in the playbook, the value will be taken from DECS_OAUTH2_URL environment variable.
+    validity:
+        description:
+        - Validity of the JWT in seconds. Default value is 3600 (one hour).
+        required: no
+    verify_ssl:
+        description:
+        - 'Controls SSL verification mode when making API calls to DECS controller. Set it to False if you
+         want to disable SSL certificate verification.'
+        - `Intended use case is when you run module in a trusted environment that uses self-signed certificates. 
+         Note that disabling SSL verification in any other scenario can lead to security issues, so please use 
+         with caution.'
+        default: True
+        required: no
 '''
 
 EXAMPLES = '''
@@ -40,7 +72,7 @@ EXAMPLES = '''
 
 RETURN = '''
 jwt:
-    description: JSON web token that can be used to access DECS cloud controller
+    description: JSON Web Token that can be used to access DECS cloud controller
     returned: always
     type: string
     sample: None
@@ -66,6 +98,9 @@ def decs_jwt_parameters():
         oauth2_url=dict(type='str',
                         required=True,
                         fallback=(env_fallback, ['DECS_OAUTH2_URL'])),
+        validity=dict(type='int',
+                      required=False,
+                      default=3600),
         verify_ssl=dict(type='bool', required=False, default=True),
         workflow_callback=dict(type='str', required=False),
         workflow_context=dict(type='str', required=False),
@@ -84,7 +119,7 @@ def main():
                     client_id=amodule.params['app_id'],
                     client_secret=amodule.params['app_secret'],
                     response_type="id_token",
-                    validity=1200,)
+                    validity=amodule.params['validity'],)
     # TODO: Need standard code snippet to handle server timeouts gracefully
     # Consider a few retries before giving up or use requests.Session & requests.HTTPAdapter
     # see https://stackoverflow.com/questions/15431044/can-i-set-max-retries-for-requests-request
